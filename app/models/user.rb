a41016a79a3e6fcca333746aad1868a20051f328
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2022-05-29 20:08:49 +0900

    userモデル、バリデーション・アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  validates :nickname,     presence: true
+  validates :phone_number, presence: true
+
+  has_many :recruitments
+  has_many :agreements
 end
